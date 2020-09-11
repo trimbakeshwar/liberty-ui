2ee4bcc43c3cde0efd09d73ad10fca3dfca0a5a7
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-09-11 14:10:48 +0530

    Added:-welcome to liberty-ui

---
+++ src/component/dashbord.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import "../styles/dashbordstyle.scss"
+class Dashbord extends Component{
+    render(){
+        return(
+            <div className="dashbord">
+                <h1>hello</h1>
+               
+            </div>
+        );
+    }
+}
+export default  Dashbord
---
+++ src/component/headers.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class headers extends Component{
+    render(){
+        return(
+            <div className="">
+               
+            </div>
+        );
+    }
+}
+export default  headers
